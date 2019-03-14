aa2f022280f047b110cbdbad44cad78c7b834938
Yuvaraj D <yuva951.dev@gmail.com>
2019-03-14 09:54:14 -0700

    Adding cors and Json serializer

--- BackOffice.Core/BackOffice.Core/Startup.cs
+++ BackOffice.Core/BackOffice.Core/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace BackOffice.Core
 {
@@ -26,8 +27,16 @@ namespace BackOffice.Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(options => {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if(resolver != null)
+                    {
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                    }
+                });
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +47,10 @@ namespace BackOffice.Core
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(options => 
+            options.WithOrigins("http://localhost:4200/").
+            AllowAnyMethod()
+            .AllowAnyHeader());
             app.UseMvc();
         }
     }
